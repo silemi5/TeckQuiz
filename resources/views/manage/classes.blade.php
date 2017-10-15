9483ced8280c4d6138bc356af1143e4fb425b6df
silemi5 <sileofthefive@gmail.com>
2017-10-15 16:35:57 +0800

    Resetting of password of students via teacher implemented.

--- resources/views/manage/classes.blade.php
+++ resources/views/manage/classes.blade.php
@@ -1,56 +1,57 @@
-@extends('layouts.app')
-@section('title', 'Manage class - TeckQuiz')
-@section('content')
-    <style>
-        body {
-            padding-top: 70px;
-        }
-    </style>
-    <div class="container-fluid">
-        <div class="row">
-            <nav class="col-sm-3 col-md-2 d-none d-sm-block bg-light sidebar">
-                <ul class="nav nav-pills flex-column">
-                    <li class="nav-item">
-                        <a class="nav-link active" id="v-pills-class" data-toggle="pill" href="#class-tab" role="tab" aria-controls="v-pills-class"
-                            aria-expanded="true">Class</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" id="v-pills-class" data-toggle="pill" href="#quiz-tab" role="tab" aria-controls="v-pills-quizzes"
-                            aria-expanded="true">Quizzes</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" id="v-pills-students" data-toggle="pill" href="#students-tab" role="tab" aria-controls="v-pills-students"
-                            aria-expanded="true">Students</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" id="v-pills-settings" data-toggle="pill" href="#settings-tab" role="tab" aria-controls="v-pills-settings"
-                            aria-expanded="true">Settings</a>
-                    </li>
-                </ul>
-            </nav>
+@extends('layouts.app') @section('title', 'Manage class - TeckQuiz') @section('content')
+<style>
+    body {
+        padding-top: 70px;
+    }
+</style>
+<div class="container-fluid">
+    <div class="row">
+        <nav class="col-sm-3 col-md-2 d-none d-sm-block bg-light sidebar">
+            <ul class="nav nav-pills flex-column">
+                <li class="nav-item">
+                    <a class="nav-link active" id="v-pills-class" data-toggle="pill" href="#class-tab" role="tab" aria-controls="v-pills-class"
+                        aria-expanded="true">Class</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="v-pills-class" data-toggle="pill" href="#quiz-tab" role="tab" aria-controls="v-pills-quizzes" aria-expanded="true">Quizzes</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="v-pills-students" data-toggle="pill" href="#students-tab" role="tab" aria-controls="v-pills-students"
+                        aria-expanded="true">Students</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="v-pills-settings" data-toggle="pill" href="#settings-tab" role="tab" aria-controls="v-pills-settings"
+                        aria-expanded="true">Settings</a>
+                </li>
+            </ul>
+        </nav>
 
-            <main class="col-sm-9 ml-sm-auto col-md-10 pt-3" role="main">             
-                <h3>{{ $quiz_class->subject->subject_code }}: {{ $quiz_class->subject->subject_desc }}</h3>
-                <h5><span class="badge badge-primary">{{ $quiz_class->course_sec }}</span></h5>
-                <hr>
+        <main class="col-sm-9 ml-sm-auto col-md-10 pt-3" role="main">
+            <h3>{{ $quiz_class->subject->subject_code }}: {{ $quiz_class->subject->subject_desc }}</h3>
+            <h5>
+                <span class="badge badge-primary">{{ $quiz_class->course_sec }}</span>
+            </h5>
+            <hr>
 
-                <div class="tab-content col" id="v-pills-tabContent">
-                    <div class="tab-pane fade show active row" id="class-tab" role="tabpanel" aria-labelledby="class-tab">
-                        <div class="row">
-                            <div class="col-3">
-                                <div class="card text-white bg-primary">
-                                    <div class="card-body">
-                                        <p class="h1 text-center">{{ $quiz_events->count() }}</p>
-                                        <p class="text-center"> Quiz{{ $quiz_events->count() <= 1 ? '' : 'zes' }}</p>
-                                    </div>
-                                    <a class="card-footer text-white clearfix small z-1 text-center" href="">View quiz</a>
+            <div class="tab-content col" id="v-pills-tabContent">
+                <div class="tab-pane fade show active row" id="class-tab" role="tabpanel" aria-labelledby="class-tab">
+                    <div class="row">
+                        <div class="col-3">
+                            <div class="card text-white bg-primary">
+                                <div class="card-body">
+                                    <p class="h1 text-center">{{ $quiz_events->count() }}</p>
+                                    <p class="text-center"> Quiz{{ $quiz_events->count()
+                                        <=1 ? '' : 'zes' }}</p>
                                 </div>
+                                <a class="card-footer text-white clearfix small z-1 text-center" href="">View quiz</a>
                             </div>
-                            <div class="col-3">
-                                <div class="card text-white bg-success">
-                                    <div class="card-body">
-                                        <p class="h1 text-center">{{ $students->count() }}</p>
-                                        <p class="text-center"> Student{{ $quiz_events->count() < 1 ? '' : 's' }}</p>
+                        </div>
+                        <div class="col-3">
+                            <div class="card text-white bg-success">
+                                <div class="card-body">
+                                    <p class="h1 text-center">{{ $students->count() }}</p>
+                                    <p class="text-center"> Student{{ $quiz_events->count()
+                                        < 1 ? '' : 's' }}</p>
                                     </div>
                                     <a class="card-footer text-white clearfix small z-1 text-center" href="">View student list</a>
                                 </div>
@@ -164,11 +165,14 @@
                                 <div class="form-group">
                                     <label>Name:</label>
                                     <div class="form-inline">
-                                        <input type="text" class="form-control m-1" placeholder="Family Name" id="f-name" style="width:12rem">
-                                        <input type="text" class="form-control m-1" placeholder="Given Name" id="g-name" style="width:12rem">
-                                        <input type="text" class="form-control m-1" placeholder="M.I." id="mi-name" style="width:4rem">
-                                        <input type="text" class="form-control m-1" placeholder="Extension Name" id="ne-name" style="width:12rem">
+                                        <input type="text" class="form-control m-1" placeholder="Family Name" id="f-name" style="width:12rem" disabled>
+                                        <input type="text" class="form-control m-1" placeholder="Given Name" id="g-name" style="width:12rem" disabled>
+                                        <input type="text" class="form-control m-1" placeholder="M.I." id="mi-name" style="width:4rem" disabled>
+                                        <input type="text" class="form-control m-1" placeholder="Extension Name" id="ne-name" style="width:12rem" disabled>
                                     </div>
+                                </div>
+                                <div class="form-group">
+                                    <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#resetStudentPassword">Reset password of this user</button>
                                 </div>
                             </form>
                         </div>
@@ -199,92 +203,126 @@
                     </div>
                 </div>
             </div>
-
-            <!-- <div class="modal fade" id="AddNewStudentModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog modal-lg" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Add New Student</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            <form class="form">
-                                <input type="hidden" id="usrid" value="-1">
-                                <div class="form-group">
-                                    <label>Name:</label>
-                                    <div class="form-inline">
-                                        <input type="text" class="form-control m-1" placeholder="Family Name" id="f-name" style="width:12rem">
-                                        <input type="text" class="form-control m-1" placeholder="Given Name" id="g-name" style="width:12rem">
-                                        <input type="text" class="form-control m-1" placeholder="M.I." id="mi-name" style="width:4rem">
-                                        <input type="text" class="form-control m-1" placeholder="Extension Name" id="ne-name" style="width:12rem">
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary" id="UpdateProfile">Update Profile</button>
-                        </div>
-                    </div>
-                </div>
-            </div> -->
-
-            <script>
-                $('#StudentProfileModal').on('show.bs.modal', function (event) {
-                    var button = $(event.relatedTarget) // Button that triggered the modal
-                    var sid = button.data('usrid')
-                    var gname = button.data('gname')
-                    var fname = button.data('fname')
-                    var miname = button.data('miname')
-                    var nename = button.data('nename')
-                    var act = button.data('act')
-
-                    var modal = $(this)
-                    modal.find('#g-name').val(gname)
-                    modal.find('#f-name').val(fname)
-                    modal.find('#mi-name').val(miname)
-                    modal.find('#ne-name').val(nename)
-                    modal.find('#usrid').val(sid)
-                });
-
-                $('#UpdateProfile').click(function (){
-                    var modal = $(this)
-                    var g = $('#g-name').val()
-                    var f = $('#f-name').val()
-                    var mi = $('#mi-name').val()
-                    var ne = $('#ne-name').val()
-                    var sid = $('#usrid').val()
-                    var act = $('#act').val()
-                    
-                    $.ajax({
-                        url: '/student/update',
-                        type: 'POST',
-                        data: {g, f, mi, ne, sid, act},
-                        success: function(result) {
-                            location.reload(true);
-                        }
-                    });
-                });
-
-                $.ajaxSetup({
-                    headers: {
-                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                    }
-                });
-
-                function deleteClass(id){
-                    $.ajax({
-                        url: '/class/' + id,
-                        type: 'DELETE', //type is any HTTP method
-                        success: function () {
-                            window.location = "/panel"
-                        }
-                    });
-                }
-
-            </script>
         </div>
     </div>
+
+
+    <!-- Reset Student password Success Modal -->
+    <div class="modal fade" id="resetStudentPasswordSuccess" tabindex="-1" role="dialog" aria-labelledby="resetStudentPasswordSuccess"
+        aria-hidden="true">
+        <div class="modal-dialog modal-sm" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Successful reset of password</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    Success! The password is reseted to:
+                    <b>password</b>
+                    <input type="hidden" id="t_id" value="">
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <!-- Reset Student password Confirmation Modal -->
+    <div class="modal fade" id="resetStudentPassword" tabindex="-1" role="dialog" aria-labelledby="resetStudentPassword" aria-hidden="true">
+        <div class="modal-dialog modal-sm" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Confirm reset of password</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    Are you sure to reset this teacher's password?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-danger" onclick="ResetStudentPassword()">Reset password</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<script>
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+
+    $('#StudentProfileModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget) // Button that triggered the modal
+        var sid = button.data('usrid')
+        var gname = button.data('gname')
+        var fname = button.data('fname')
+        var miname = button.data('miname')
+        var nename = button.data('nename')
+        var act = button.data('act')
+
+        var modal = $(this)
+        modal.find('#g-name').val(gname)
+        modal.find('#f-name').val(fname)
+        modal.find('#mi-name').val(miname)
+        modal.find('#ne-name').val(nename)
+        modal.find('#usrid').val(sid)
+    });
+
+    /*$('#UpdateProfile').click(function (){
+        var modal = $(this)
+        var g = $('#g-name').val()
+        var f = $('#f-name').val()
+        var mi = $('#mi-name').val()
+        var ne = $('#ne-name').val()
+        var sid = $('#usrid').val()
+        var act = $('#act').val()
+        
+        $.ajax({
+            url: '/student/update',
+            type: 'POST',
+            data: {g, f, mi, ne, sid, act},
+            success: function(result) {
+                location.reload(true);
+            }
+        });
+    });*/
+
+    function deleteClass(id) {
+        $.ajax({
+            url: '/class/' + id,
+            type: 'DELETE', //type is any HTTP method
+            success: function () {
+                window.location = "/panel"
+            }
+        });
+    }
+
+    function ResetStudentPassword() {
+        var s_id = $('#usrid').val();
+        var password = "password";
+        var update_type = 1;
+        
+            $.ajax({
+                url: '/account/' + s_id,
+                type: 'PUT', //type is any HTTP method
+                data: {
+                    update_type, password
+                }, //Data as js object
+                success: function () {
+                    $('#resetStudentPassword').modal('hide')
+                    $('#resetStudentPasswordSuccess').modal('show')
+                }
+            });
+        
+        alert(s_id);
+    }
+
+</script>
 @endsection
