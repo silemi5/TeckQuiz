ab28b33c2245e6a96a93ba4dc85faed52b440727
silemi5 <sileofthefive@gmail.com>
2017-10-16 14:26:49 +0800

    Revert "Indention fix on app.blade.php"
    This reverts commit 2a20fa5090b533b305459d2edb3115a0809648f7.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,17 +58,17 @@
                             {{ Auth::user()->usr }}
                             </a>
                             <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                <h6 class="dropdown-header">Signed in as: <b>{{ Auth::user()->usr }}</b></h6>
-                                <a class="dropdown-item" href="/profile">Profile</a>
-                                <a class="dropdown-item" href="/panel">User Panel</a>
-                                <a class="dropdown-item" href="#">Preference</a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                    Logout
-                                </a>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    {{ csrf_field() }}
-                                </form>
+                            <h6 class="dropdown-header">Signed in as: <b>{{ Auth::user()->usr }}</b></h6>
+                            <a class="dropdown-item" href="/profile">Profile</a>
+                            <a class="dropdown-item" href="/panel">User Panel</a>
+                            <a class="dropdown-item" href="#">Preference</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                Logout
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                {{ csrf_field() }}
+                            </form>
                             </div>
                         </li>
                     </ul>
